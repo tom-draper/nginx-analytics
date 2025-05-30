07ecdb58a1403acee9318bc873a017ebe8fc9506
Tom Draper <tomjdraper1@gmail.com>
2025-05-30 16:49:37 +0100

    feat: dashboard layout

--- cli/internal/ui/styles/colors.go
+++ cli/internal/ui/styles/colors.go
@@ -3,9 +3,9 @@ package styles
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	BorderColor = lipgloss.Color("238") // Gray
+	BorderColor = lipgloss.Color("238")
 	BorderColor2 = lipgloss.Color("63")
 	Gray        = lipgloss.Color("245")
 	LightGray   = lipgloss.Color("241")
-	Blue        = lipgloss.Color("39") // Blue
+	Blue        = lipgloss.Color("39")
 )
