a9577244236e5edae1649a344ba2c6c5749ea724
Tom Draper <tomjdraper1@gmail.com>
2025-05-30 09:47:25 +0100

    feat: refactor agent config

--- cli/main.go
+++ cli/main.go
@@ -6,12 +6,15 @@ import (
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/tom-draper/nginx-analytics/agent/pkg/config"
 
 	"github.com/tom-draper/nginx-analytics/cli/internal/model"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+
+	cfg := config.LoadConfig()
 
 	p := tea.NewProgram(model.New(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
