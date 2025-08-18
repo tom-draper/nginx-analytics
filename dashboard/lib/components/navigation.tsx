93ae268178e76ea9928bda750ce1f74528c1954e
Tom Draper <tomjdraper1@gmail.com>
2025-08-18 16:56:08 +0100

    feat: improve get started hover animation and make more consistent

--- dashboard/lib/components/navigation.tsx
+++ dashboard/lib/components/navigation.tsx
@@ -26,8 +26,9 @@ export function Navigation({ filterPeriod, setFilterPeriod, setShowSettings, isD
                                 stroke="currentColor"
                                 className="size-6 h-[18px] my-auto ml-[6px] 
                            transition-transform duration-300 ease-in-out 
-                           transition-colors delay-300 
                            group-hover:translate-x-[2px] 
+                           group-hover:transition-colors 
+                           group-hover:delay-300 
                            group-hover:stroke-[var(--highlight)]"
                             >
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"></path>
